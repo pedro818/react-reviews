a9b6de068f8785482161d1c800d359f200dde66b
Pedrocode <pedro_cuello101@hotmail.com>
2021-01-30 18:11:48 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      text:
+        "Un Web Developer es aquella persona encargada de programar el navegador de una web, es decir, es el encargado de traducir las definiciones de diseño y estilo visual realizadas en etapas previas a códigos HTML semántico.",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+      text:
+        'Es quien se encarga de la identidad visual, la coherencia y consistencia gráfica del sitio web, priorizando la comunicación y una interfaz de usuario responsiva; seleccionando los elementos que la componen y codificando los diseños en lenguajes HTML, CSS y JS.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      text:
+        'Como persona organizada y con una gran motivación, soy capaz de adaptarme a cualquier circunstancia y dar siempre lo mejor de mí en cualquier proyecto, al mismo tiempo que me esfuerzo por trabajar en equipo y fomentar valores como los del compañerismo.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      text:
+        'Soy el responsable de la dirección de un determinado proyecto, encargado de la consecución de objetivos determinados y de la organización de un equipo para la misma. En los equipos profesionales habituales en ámbitos económicos y organizacionales, la figura del jefe existe por autoridad formal.',
+    },
+  ];
+  
+  export default reviews;
